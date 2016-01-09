385065b634190ad15cdc386061eb5e0740fad6a8
Ali Polatel <alip@exherbo.org>
2016-01-09 15:04:59 +0200

    stem nltk betiği düzenlendi

--- bin/stem-porter.py
+++ bin/stem-porter.py
@@ -10,4 +10,7 @@ stem_word = lambda word: PS.stem(word)
 if __name__ == '__main__':
     for line in sys.stdin:
         word = line.strip()
-        print(stem_word(word))
+        if word == '000':
+            sys.exit(0)
+        sys.stdout.write(stem_word(word) + "\n")
+        sys.stdout.flush()
